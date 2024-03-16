ba225b5df2b834c01467aef93cfbd5a8bf841d48
gowthami1991 <163662599+gowthami1991@users.noreply.github.com>
2024-03-16 20:42:59 +0530

    Update ChatBird.py

--- ChatBird.py
+++ ChatBird.py
@@ -34,7 +34,7 @@ option2 = st.sidebar.selectbox('Select The Downloaded Option ', ['View', 'Downlo
 if submit_button:
     try:
         # SQLite database connection
-        conn = sqlite3.connect('E:/customer.db')
+        conn = sqlite3.connect('chatbrid/customer.db')
 
         if search_term:
             # Specify the columns to search in
